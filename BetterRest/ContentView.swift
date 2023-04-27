ab3e3ed6c2c1a5a0f7ebdd7324ddb0ecfcb3de39
Alex Smith <alexwsmith@me.com>
2023-04-26 20:10:47 -0700

    Small tweak. Set default wake time to 7 AM

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,13 +8,20 @@ import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
+    
+   static var defaultWakeTime: Date {
+        var componets = DateComponents()
+        componets.hour = 7
+        componets.minute = 0
+        return Calendar.current.date(from: componets) ?? Date.now
+    }
     
     var body: some View {
         NavigationView {
@@ -39,34 +46,32 @@ struct ContentView: View {
             .navigationTitle("Better Rest")
             .toolbar {
                 Button("Calculate", action: calculateBedTime)
-            }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("Ok") {
-                } message: {
-                    Text(alertMessage)
-                }
+            } .alert(alertTitle, isPresented: $showingAlert) {
+                Button("Ok") {}
+            } message: {
+                Text(alertMessage)
             }
         }
-        func calculateBedTime() {
-            do {
-                let config = MLModelConfiguration()
-                let model = try SleepCalculator(configuration: config)
-                let componets =  Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
-                let hour = (componets.hour ?? 0) * 60 * 60
-                let minute = (componets.minute ?? 0) * 60
-                
-                let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-                
-                let sleepTime = wakeUp - prediction.actualSleep
-                alertTitle = "Your ideal bedtime is ..."
-                alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
-                
-            } catch {
-                alertTitle = "Error"
-                alertMessage = "Sorry, there was a problem calculating your bedtime."
-            }
-            showingAlert = true
+    }
+    func calculateBedTime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let componets =  Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (componets.hour ?? 0) * 60 * 60
+            let minute = (componets.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is ..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
         }
+        showingAlert = true
     }
 }
 struct ContentView_Previews: PreviewProvider {
